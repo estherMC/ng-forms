14b5a5719c99072999f83fcfd2a882a4ccd213b3
Esther Martorell <esther.martorell@allianz.com>
2023-07-15 20:02:12 +0200

    :sparkles: access to object created in the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  //Al afegit el ngForm a f, ja no és un HTMLFormElement, sinó NgForm
+  // onSubmit(form: HTMLFormElement){
+  onSubmit(form: NgForm){
+    console.log(form);
   }
 }
