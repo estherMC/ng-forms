48d82d45b92e0bf9f3fb1674db4aca9df5977c56
Esther Martorell <ster.cat@gmail.com>
2023-07-16 16:29:24 +0200

    :sparkles: set partially fields of the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,21 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    //Amb el següent exemple sobreescribim tot el contingut
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ' '
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'female'
+    // })
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   //Al afegit el ngForm a f, ja no és un HTMLFormElement, sinó NgForm
